436c1f9a6a41f9ba59dc306d140d339b218c883c
Eduardo Cardoso <ygarasab@gmail.com>
2021-05-03 17:30:56 -0300

    feat : indicator correlation value by counrty added

--- scripts/issue1.js
+++ scripts/issue1.js
@@ -7,13 +7,11 @@ const gdpIndicatorArray = data => {
     let gdps = data.map(instance => parseInt(instance[gdpKey]))
     gdps = gdps.filter(value => !isNaN(value)).sort((a,b) => a-b)
     gdps = [... new Set(gdps)]
-    console.log(gdps)
+
 
     let indicatorsValues = {}
 
-    let mustAvoid = [ gdpKey, 'year', "Country Name", "Country Code", "Surface area (sq. km)", "Net migration", "Rural population living in areas where elevation is below 5 meters (% of total population)", "Employment in agriculture, female (% of female employment) (modeled ILO estimate)", "Employment in agriculture, male (% of male employment) (modeled ILO estimate)",
-    "Arable land (hectares)", "Arable land (% of land area)", "Agricultural irrigated land (% of total agricultural land)", "Cereal production (metric tons)", "Agriculture, forestry, and fishing, value added (current US$)", "Fertilizer consumption (% of fertilizer production)", "Rural land area where elevation is below 5 meters (sq. km)",
-    "Crop production index (2004-2006 = 100)", "Rural population growth (annual %)"]
+    let mustAvoid = [ gdpKey, 'year', "Country Name", "Country Code", "Surface area (sq. km)"]
 
     for(let i of data.columns){
         if( mustAvoid.includes(i) ) continue
@@ -38,6 +36,55 @@ const gdpIndicatorArray = data => {
         correlations[i] = stats.correlationCoefficient('gdp', 'value').correlationCoefficient
     }
 
-    console.log(correlations)
+    console.log(JSON.stringify(correlations))
+
+}
+
+const getIndicatorToGdpCorrelationPerCountry = (data, columns, indicator) => {
+
+    let gdpKey = 'GDP per capita (current US$)'
+
+    let result = {}
+
+    let countries = [...new Set(data.map(instance => instance['Country Code']))]
+
+    for(let country of countries){
+
+        let countryData = data.filter(instance => instance['Country Code'] === country)
+
+        let gdps = countryData.map(instance => parseInt(instance[gdpKey]))
+        gdps = gdps.filter(value => !isNaN(value)).sort((a,b) => a-b)
+        gdps = [... new Set(gdps)]
+
+
+        let indicatorsValues = []
+
+
+        for(let gdp of gdps){
+
+            let target =  countryData.filter(instance => parseInt(instance[gdpKey]) === gdp)
+
+            let value = target.map(instance => parseFloat(instance[indicator])).filter(instance => !isNaN(instance))
+            if(value.length) indicatorsValues.push({gdp, value : average(value)})
+
+
+        }
+
+        let correlations = {}
+
+        for(let i in indicatorsValues){
+            if(indicatorsValues.length > 0){
+                let stats = new Statistics(indicatorsValues, {gdp:'metric',value:'metric'})
+                correlations = stats.correlationCoefficient('gdp', 'value').correlationCoefficient
+            }
+            else correlations = NaN
+        }
+
+        result[country] = correlations
+
+    }
+
+    return result
+
 
 }
